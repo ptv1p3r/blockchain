ce7788a468ef1650e8594f8a8ccb362a634c1c8d
Pedro Roldan <pedro.roldang@mail.com>
2020-05-26 22:12:17 +0100

    deleted cypher

--- cypher/teste.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-print('Number of arguments:', len(sys.argv), 'arguments.')
-
-print('Argument List:', str(sys.argv[1]))
-
-for index in range(len(sys.argv)):
-    print('Number of arguments:', index, 'arguments.')

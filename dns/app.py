d10a98f5d03295e4573e8aacd6903faf70c796c3
Paulo Craveiro <pauloe.craveiro@hotmail.com>
2020-05-27 02:18:49 +0100

    @app.route("/") added to file app.py

--- dns/app.py
+++ dns/app.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask, jsonify
 from dns.config import *
 from dns.routes import *
@@ -5,6 +6,13 @@ from dns.routes import *
 app = Flask(__name__)
 # Regista o endpoint dnsRoute
 app.register_blueprint(dnsRoute)
+
+
+# Aviso no IP 127.0.0.1
+@app.route("/")
+def index():
+    return "Computação Distribuida : BlockChain" + ' {0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+
 
 if SERVER_MODE_DEV:
     if __name__ == '__main__':

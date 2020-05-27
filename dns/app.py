b812aff0494d989583148966b6318f16b39b2f5f
Paulo Craveiro <pauloe.craveiro@hotmail.com>
2020-05-27 01:19:58 +0100

    testing errors

--- dns/app.py
+++ dns/app.py
@@ -1,11 +1,13 @@
-from flask import Flask
-from dns import routes
+from flask import Flask, jsonify
 from dns.config import *
 
 app = Flask(__name__)
 
-
-
 if SERVER_MODE_DEV:
     if __name__ == '__main__':
         app.run(host=SERVER_HOST, port=SERVER_PORT, debug=True)
+
+
+@app.route('/helo')
+def hello():
+    return jsonify({'message': "Hello"})
--- dns/routes.py
+++
@@ -1,7 +0,0 @@
-from dns import app
-from flask import jsonify
-
-
-@app.route('/hello')
-def hello():
-    return jsonify({'message': "Hello"})
